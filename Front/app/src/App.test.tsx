6e1705d87c8984037fa708fd59ea041f50b9212a
松野浩士 <amanojac52@gmail.com>
2021-01-09 22:07:31 +0900

    テストの更新 #9

--- Front/app/src/App.test.tsx
+++ Front/app/src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('お題の文字が画面に含まれるか', () => {
+test('お題セットボタンがあるか', () => {
   render(<App />);
-  const linkElement = screen.getByText(/お題/i);
+  const linkElement = screen.getByText(/お題をボードにセットする/i);
   expect(linkElement).toBeInTheDocument();
 });
